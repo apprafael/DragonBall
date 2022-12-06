263ec9cb6d01854110a34661224605cc5ba79837
Rafael Almeida <apprafael.almeida@gmail.com>
2022-12-06 15:34:02 -0300

    Initial Commit

---
+++ DragonBall/DragonBallApp.swift
@@ -0,0 +1,17 @@
+//
+//  DragonBallApp.swift
+//  DragonBall
+//
+//  Created by Rafael Almeida on 12/6/22.
+//
+
+import SwiftUI
+
+@main
+struct DragonBallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
