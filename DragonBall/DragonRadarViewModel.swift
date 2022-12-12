97f2ed1041e519d06ee84def8e65ee007481a88d
Rafael Almeida <apprafael.almeida@gmail.com>
2022-12-12 16:15:14 -0300

    get magnetic heading

--- DragonBall/DragonRadarViewModel.swift
+++ DragonBall/DragonRadarViewModel.swift
@@ -6,13 +6,26 @@
 //
 
 import Foundation
+import CoreLocation
 
-class DragonRadarViewModel: ObservableObject {
+class DragonRadarViewModel: NSObject, ObservableObject {
+    private var locationManager: CLLocationManager?
     var dragonBalls = [DragonBall]()
     
-    init() {        
+    override init() {
+        super.init()
+        locationManager = CLLocationManager()
+        locationManager?.requestAlwaysAuthorization()
+        locationManager?.delegate = self
+        locationManager?.startUpdatingHeading()
         dragonBalls.append(DragonBall(positionX: 70, positionY: 300))
         dragonBalls.append(DragonBall(positionX: -100, positionY: 70))
         dragonBalls.append(DragonBall(positionX: 100, positionY: -60))
+    }
+}
+
+extension DragonRadarViewModel: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateHeading newHeading: CLHeading) {
+        print(newHeading.magneticHeading)
     }
 }
